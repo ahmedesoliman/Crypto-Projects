afaaee443f59cf9ab6234fcc231b59b9326123ae
ahmedesoliman <ahmedesolimans@gmail.com>
2022-12-17 21:38:55 -0500

    update

--- RSA-Project/Project_01.py
+++ RSA-Project/Project_01.py
@@ -1,29 +1,34 @@
-import random, math, itertools
+import random
+import math
+import itertools
 
 # Generate random prime number with n bits.
-def MRT(bits): # bits is the number of bits in the prime number
-    get_random_t = lambda: random.getrandbits(bits) | 1 << bits | 1 # get a random number with bits length and make sure it is odd and greater than 2
-    p = get_random_t() # p is a random number with bits length and make sure it is odd and greater than 2
-    for i in itertools.count(1): # for i in count 1 to infinity
-        if isPrime(p): # if p is prime then 
-            return p # return p
-        else: 
-            if i % (bits * 2) == 0: # if i is divisible by bits*2 then 
-             p = get_random_t() # p is a random number with bits length and make sure it is odd and greater than 2
+def MRT(bits):  # bits is the number of bits in the prime number
+    # get a random number with bits length and make sure it is odd and greater than 2
+    def get_random_t(): return random.getrandbits(bits) | 1 << bits | 1 #this is like 2^(bits-1)+2^(bits)+1
+    # p is a random number with bits length and make sure it is odd and greater than 2
+    p = get_random_t()
+    for i in itertools.count(1):  # for i in count 1 to infinity
+        if isPrime(p):  # if p is prime then
+            return p  # return p
+        else:
+            if i % (bits * 2) == 0:  # if i is divisible by bits*2 then
+                # p is a random number with bits length and make sure it is odd and greater than 2
+                p = get_random_t()
             else:
-                p += 2 # Add 2 since we are only interested in odd numbers
+                p += 2  # Add 2 since we are only interested in odd numbers
 
-#imolement millerTest function to test if a number is prime or not 
-def miillerTest(d, n): # 
+# imolement millerTest function to test if a number is prime or not
+def miillerTest(d, n):
     # Pick a random number in [2..n-2]
     # Corner cases make sure that n > 4
-    a = 2 + random.randint(1, n - 4);
+    a = 2 + random.randint(1, n - 4)
 
     # Compute a^d % n
-    x = powmod_sm(a, d, n);
+    x = powmod_sm(a, d, n)
 
     if (x == 1 or x == n - 1):
-        return True;
+        return True
 
     # Keep squaring x while one
     # of the following doesn't
@@ -32,35 +37,36 @@ def miillerTest(d, n): #
     # (ii) (x^2) % n is not 1
     # (iii) (x^2) % n is not n-1
     while (d != n - 1):
-        x = (x * x) % n;
-        d *= 2;
+        x = (x * x) % n
+        d *= 2
 
         if (x == 1):
-            return False;
+            return False
         if (x == n - 1):
-            return True;
+            return True
 
         # Return composite
-        return False;
+        return False
 
-def isPrime( n, k=100):
+# This function is called for all k trials. It returns false if n is composite and returns false if n is probably prime. d is an odd number such that d*2<sup>r</sup> = n-1 for some r >= 1
+def isPrime(n, k=100):
     # Corner cases
-    if (n <= 1 or n == 4):
-        return False;
-    if (n <= 3):
-        return True;
+    if (n <= 1 or n == 4): # 1 is not prime and 4 is not prime
+        return False
+    if (n <= 3): # 2 and 3 are prime
+        return True
 
     # Find r such that n = 2^d * r + 1 for some r >= 1
-    d = n - 1;
-    while (d % 2 == 0):
-        d //= 2;
+    d = n - 1
+    while (d % 2 == 0): # while d is even
+        d //= 2 
 
     # Iterate given nber of 'k' times
     for i in range(k):
-        if (miillerTest(d, n) == False):
-            return False;
+        if (miillerTest(d, n) == False): # if n is composite
+            return False
 
-    return True;
+    return True
 
 # Returns pair (x, y) such that xa + yb = gcd(a, b)
 def EEA(a, b):
@@ -71,57 +77,63 @@ def EEA(a, b):
         a, b = b, r
         x, lastx = lastx - q * x, x
         y, lasty = lasty - q * y, y
-        
+
     return lastx, lasty
 
-#   """Find the multiplicative inverse of e mod n."""
+# Find the multiplicative inverse of e mod n.
 def multiplicative_inverse(e, n):
-
     x, y = EEA(e, n)
     if x < 0:
         return n + x
 
     return x
 
-#"""Calculate x^m modulo n using O(log(m)) operations."""
-def powmod_sm(x, m, n): # x is the base , m is the exponent and n is the modulus
-    a = 1 # a is the result
-    while m > 0: # while m is greater than 0
-        if m % 2 == 1: # if m is odd
-            a = (a * x) % n # a = a*x mod n
-        x = (x * x) % n # x = x^2 mod n
-        m //= 2 # m = m/2
+# Calculate x^m modulo n using O(log(m)) operations.
+def powmod_sm(x, m, n):  # x is the base , m is the exponent and n is the modulus
+    a = 1  # a is the result
+    while m > 0:  # while m is greater than 0
+        if m % 2 == 1:  # if m is odd
+            a = (a * x) % n  # a = a*x mod n
+        x = (x * x) % n  # x = x^2 mod n
+        m //= 2  # m = m/2
 
     return a
 
-def rsa_generate_key(bits): # bits is the number of bits in the prime number
-    p = MRT(bits // 2) # p is a random prime number with bits/2 length
-    q = MRT(bits // 2) # q is a random prime number with bits/2 length
+# Generate a public/private key pair. The public key is (n, e) and the private key is (n, d).
+def rsa_generate_key(bits):  # bits is the number of bits in the prime number
+    p = MRT(bits // 2)  # p is a random prime number with bits/2 length
+    q = MRT(bits // 2)  # q is a random prime number with bits/2 length
     # Ensure q != p, though for large values of bits this is
     # statistically very unlikely
-    while q == p: # while q is equal to p
-        q = MRT(bits // 2) # q is a random prime number with bits/2 length
-    
-    n = p * q # n = p*q
-    phi = (p - 1) * (q - 1) # phi = (p-1)*(q-1)
+    while q == p:  # while q is equal to p
+        q = MRT(bits // 2)  # q is a random prime number with bits/2 length
+
+    n = p * q  # n = p*q
+    phi = (p - 1) * (q - 1)  # phi = (p-1)*(q-1)
     # Here we pick a random e, but a fixed value for e can also be used.
     while True:
-        e = random.randint(3, phi - 1) # e is a random number between 3 and phi-1
-        if math.gcd(e, phi) == 1: # if e and phi are coprime
+        # e is a random number between 3 and phi-1
+        e = random.randint(3, phi - 1)
+        if math.gcd(e, phi) == 1:  # if e and phi are coprime
             break
-    d = multiplicative_inverse(e, phi) # d is the multiplicative inverse of e mod phi
-    
+    # d is the multiplicative inverse of e mod phi
+    d = multiplicative_inverse(e, phi)
+
     return (n, e, d)
 
 
-def rsa_encrypt(message, n, e): # message is the message to be encrypted , n is the modulus and e is the public key
-    return powmod_sm(message, e, n) # return the encrypted message
+# message is the message to be encrypted , n is the modulus and e is the public key
+def rsa_encrypt(message, n, e):
+    return powmod_sm(message, e, n)  # return the encrypted message
 
-def rsa_decrypt(cipher, n, d): # cipher is the encrypted message , n is the modulus and d is the private key
-    return powmod_sm(cipher, d, n) # return the decrypted message
+# cipher is the encrypted message , n is the modulus and d is the private key
+def rsa_decrypt(cipher, n, d):
+    return powmod_sm(cipher, d, n)  # return the decrypted message
 
-#Driver Code
-n , e , d = rsa_generate_key(1024) # n is the modulus , e is the public key and d is the private key
+
+# Driver Code
+# n is the modulus , e is the public key and d is the private key
+n, e, d = rsa_generate_key(1024)
 
 msg = 19191919
 
@@ -131,5 +143,4 @@ print("Plain text: ", msg)
 print("Cipher Text: ", eMsg)
 
 dMsg = rsa_decrypt(eMsg, n, d)
-print("Decrypted message: ", dMsg) #19191919
-
+print("Decrypted message: ", dMsg)  # 19191919
--- RSA-Project/Project_Final.py
+++ RSA-Project/Project_Final.py
@@ -1,108 +1,117 @@
-import random, math
+import random
+import math
 
 num = random.SystemRandom()  # used to generate random numbers
 
+# Generate a random prime number of a given bit length.
+def gen_prime(bits):  # bits is the number of bits in the prime number
+    while True:
+        # Guarantees that a is odd.
+        # this is used to generate random nun of buts length 1<<bits-1 is 2^(bits-1) and 1<<bits is 2^bits
+        a = (num.randrange(1 << bits - 1, 1 << bits) << 1) + 1
+        if miller_rabin(a):  # if a is prime then return a
+            return a
+
 # implement miller rabin test for primality test  # https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
-def MRT(n, a): # n is the number to be tested and a is the random number
-  exp = n - 1 # exp is n-1
-  while not exp & 1: # while exp is even
-    exp >>= 1 # bit shift right by 1 
+def MRT(n, a):  # n is the number to be tested and a is the random number
+    exp = n - 1  # exp is n-1
+    while not exp & 1:  # while exp is even
+        exp >>= 1  # bit shift right by 1 this is same as exp = exp/2
 
-  if pow(a, exp, n) == 1: # used pow() function to calculate a^exp mod n beacuse it is faster than normal method
-    return True # if a^exp mod n is equal to 1 then return true
+    if pow(a, exp, n) == 1:  # used pow() function to calculate a^exp mod n beacuse it is faster than generic method
+        return True  # if a^exp mod n is equal to 1 then return true
 
-  while exp < n - 1: # while exp is less than n-1
-    if pow(a, exp, n) == n - 1: # used pow() function to calculate a^exp mod n beacuse it is faster than normal method
-      return True # if a^exp mod n is equal to n-1 then return true
+    while exp < n - 1:  # while exp is less than n-1
+        # used pow() function to calculate a^exp mod n beacuse it is faster than generic method
+        if pow(a, exp, n) == n - 1:
+            return True  # if a^exp mod n is equal to n-1 then return true
 
-    exp <<= 1 # bit shift left by 1
+        exp <<= 1  # bit shift left by 1 this is same as exp = exp*2
 
-  return False # if a^exp mod n is not equal to 1 or n-1 then return false
+    return False  # if a^exp mod n is not equal to 1 or n-1 then return false
 
 # k is the number of times we want to run the test. The higher the value of k, the more accurate the result will be. # https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
-def miller_rabin(n, k=100):  # n is the number to be tested and k is the number of times we want to run the test
-  for i in range(k): # for i in range k
-    a = num.randrange(2, n - 1) # a is a random number between 2 and n-1
-    if not MRT(n, a): # if MRT(n, a) is false then return false
-      return False
+# n is the number to be tested and k is the number of times we want to run the test
+def miller_rabin(n, k=500):
+    for i in range(k):  # for i in range k
+        a = num.randrange(2, n - 1)  # a is a random number between 2 and n-1
+        if not MRT(n, a):  # if MRT(n, a) is false then return false
+            return False
 
-  return True
+    return True
 
-# Generate a random prime number of a given bit length.
-def gen_prime(bits): # bits is the number of bits in the prime number
-  while True:
-    # Guarantees that a is odd.
-    a = (num.randrange(1 << bits - 1, 1 << bits) << 1) + 1 # 1<<bits-1 is 2^(bits-1) and 1<<bits is 2^bits
-    if miller_rabin(a): # if a is prime then return a
-      return a
-
+# define gcd function to calculate gcd of two numbers using euclidean algorithm
+def gcd(a, b):  # a is first number and b is second number
+    while b != 0:  # while b is not equal to 0
+        a, b = b, a % b  # a is b and b is a%b
+    return a  # return a
 
 # Returns pair (x, y) such that xa + yb = gcd(a, b) # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-def EEA(a, b): # a is public key and b is modulus
+def EEA(a, b):  # a is public key and b is modulus
 
-  x, lastx, y, lasty = 0, 1, 1, 0 
+    x, lastx, y, lasty = 0, 1, 1, 0
 
-  while b != 0: # while b is not equal to 0
-    q, r = divmod(a, b) # q is quotient and r is remainder
-    a, b = b, r # a is b and b is r 
-    x, lastx = lastx - q * x, x # x is lastx - q*x and lastx is x
-    y, lasty = lasty - q * y, y # y is lasty - q*y and lasty is y
+    while b != 0:  # while b is not equal to 0
+        q, r = divmod(a, b)  # q is quotient and r is remainder
+        a, b = b, r  # a is b and b is r
+        x, lastx = lastx - q * x, x  # x is lastx - q*x and lastx is x
+        y, lasty = lasty - q * y, y  # y is lasty - q*y and lasty is y
 
-  return lastx, lasty # return lastx and lasty
-
+    return lastx, lasty  # return lastx and lasty
 
 # Find the multiplicative inverse of e mod n. # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-def multiplicative_inverse(e, n): # e is public key and n is modulus
-  x, y = EEA(e, n) # x is multiplicative inverse of e mod n
-  if x < 0: # if x is negative then add n to it
-    return n + x 
-  return x
+def multiplicative_inverse(e, n):  # e is public key and n is modulus
+    x, y = EEA(e, n)  # x is multiplicative inverse of e mod n
+    if x < 0:  # if x is negative then add n to it
+        return n + x
+    return x
 
-# Generate a public/private key pair.
-def rsa_generate_key(bits): # bits is the number of bits in the prime number
-  p = gen_prime(bits) # generate a prime number of bits length
-  q = gen_prime(bits) # generate a prime number of bits length
+# implement powmod_sm function to calculate (base^expo) mod n
+def powmod_sm(base, expo, n):  # base is the base, expo is the exponent and n is the modulus
+    expoBits = bin(expo)  # convert expo to binary
+    value = base  # value is base
 
-  # Ensure q != p, though for large values of bits this is
-  # statistically very unlikely
-  while q == p: # while q is equal to p
-    q = gen_prime(bits) # generate a prime number of bits length
-  n = p * q # n is p*q
-  phi = (p - 1) * (q - 1) # phi is (p-1)*(q-1)
+    for i in range(3, len(expoBits)):  # for i in range 3 to length of expoBits
+        value = (value**2) % n  # value is value^2 mod n
+        if (expoBits[i:i + 1] == '1'):  # if expoBits[i:i+1] is 1 then
+            value = (value * base) % n  # value is value*base mod n
+
+    return value
+
+# Generate a public/private key pair. The public key is (n, e) and the private key is (n, d).
+def rsa_generate_key(bits):  # bits is the number of bits in the prime number
+    p = gen_prime(bits//2)  # generate a prime number of bits length
+    q = gen_prime(bits//2)  # generate a prime number of bits length
+
+    # Ensure q != p, though for large values of bits this is
+    # statistically very unlikely
+    while q == p:  # while q is equal to p
+        q = gen_prime(bits)  # generate a prime number of bits length
+    n = p * q  # n is p*q
+    phi = (p - 1) * (q - 1)  # phi is (p-1)*(q-1)
 
     # Here we pick a random e, but a fixed value for e can also be used.
-  while True:
-    e = random.randint(3, phi - 1) # e is a random number between 3 and phi-1
-    if math.gcd(e, phi) == 1: # if gcd(e, phi) is 1 then
-      break
-  # d * e = 1 mod phi 
-  d = multiplicative_inverse(e, phi) # computer d using extended euclidean algorithm 
-  return (n, e, d)
+    while True:
+        # e is a random number between 3 and phi-1
+        e = random.randint(3, phi - 1)
+        if math.gcd(e, phi) == 1:  # if gcd(e, phi) is 1 then
+            break
+    # d * e = 1 mod phi
+    # computer d using extended euclidean algorithm
+    d = multiplicative_inverse(e, phi)
+    return (n, e, d)
 
-#implement powmod_sm function to calculate (base^expo) mod n
-def powmod_sm(base, expo, n): # base is the base, expo is the exponent and n is the modulus
-  expoBits = bin(expo) # convert expo to binary
-  value = base # value is base
+# message is the message to be encrypted, n is modulus and e is public key
+def rsa_encrypt(message, n, e):
+    return powmod_sm(message, e, n)  # return (message^e) mod n
 
-  for i in range(3, len(expoBits)): # for i in range 3 to length of expoBits
-    value = (value**2) % n # value is value^2 mod n
-    if (expoBits[i:i + 1] == '1'): # if expoBits[i:i+1] is 1 then
-      value = (value * base) % n # value is value*base mod n
-
-  return value
+def rsa_decrypt(cipher, n, d):  # cipher is the cipher text, n is modulus and d is private key
+    return powmod_sm(cipher, d, n)  # return (cipher^d) mod n
 
 
-def rsa_encrypt(message, n, e): # message is the message to be encrypted, n is modulus and e is public key
-  return powmod_sm(message, e, n) # return (message^e) mod n
-
-
-def rsa_decrypt(cipher, n, d): # cipher is the cipher text, n is modulus and d is private key
-  return powmod_sm(cipher, d, n) # return (cipher^d) mod n
-
-
-#Driver code
+# Driver code
 print("RSA Encryptiona & Decryption program")
-n, e, d = rsa_generate_key(512)
+n, e, d = rsa_generate_key(1024)
 
 msg = 19191919
 
@@ -112,7 +121,4 @@ print("Plain text: ", msg)
 print("Cipher Text: ", eMsg)
 
 dMsg = rsa_decrypt(eMsg, n, d)
-print("Decrypted message: ", dMsg) #19191919
-
-
-
+print("Decrypted message: ", dMsg)  # 19191919
